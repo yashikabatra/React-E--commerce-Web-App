7bc525c93300a22ac72f52a8b6ca27975be29753
yashikabatra <yashika.batra07@gmail.com>
2021-02-19 01:06:28 +0530

    Adding screenshots

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,27 +1,38 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import { Card } from 'antd';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { Card } from "antd";
 import style from "../styles/login.css";
-
+import { Link } from "react-router-dom";
 
 class Login extends Component {
-    state = {  }
-    render() { 
-        return ( 
-           
-            <Card className="Card" >
-            <form>
-        <h1 className="loginheading" style={{'text-align' : 'center'}}>LOGIN</h1>
-        <input type='text'  className="form-control" placeholder='username'></input> <br></br>
-        <input type='text' className="form-control" placeholder='password'></input>
-        <br></br>
-        <button  class="btn btn-secondary btn-block" >LOGIN</button>
-      </form>
-          </Card>
-          
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <Card className='Card'>
+        <form>
+          <h1 className='loginheading' style={{ "text-align": "center" }}>
+            LOGIN
+          </h1>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='username'
+          ></input>{" "}
+          <br></br>
+          <input
+            type='password'
+            className='form-control'
+            placeholder='password'
+          ></input>
+          <br></br>
+          <Link to=''>
+            <button class='btn btn-secondary btn-block'>LOGIN</button>
+          </Link>
+        </form>
+      </Card>
+    );
+  }
 }
- 
+
 export default Login;
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -12,7 +12,7 @@ class Signup extends Component {
              <form>
         <h1 className="signup" style={{'text-align' : 'center'}}>SIGNUP</h1>
         <input type='text'  className="form-control" placeholder='username'></input> <br></br>
-        <input type='text' className="form-control" placeholder='password'></input>
+        <input type='password' className="form-control" placeholder='password'></input>
         <br></br>
         <button  class="btn btn-secondary btn-block" >Signup</button>
       </form>
